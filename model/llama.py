1. Fixed encoder and masking issues 2. used pytorch sdp and enabled flash attention

model/llama.py
@@ -3,7 +3,7 @@ from torch import nn, Tensor
 from dataclasses import dataclass
 from typing import Optional
 import torch.nn.functional as F
-from rope import RotaryPositionalEmbeddings
+from .rope import RotaryPositionalEmbeddings
 
 # Define the necessary arguments for developing LLaMA-3 from scratch
 
@@ -13,7 +13,6 @@ class LLM_Args:
     num_layers: int  # Number of transformer layers in the model
     num_heads: int  # Number of attention heads in each transformer layer
     embed_dim: int  # Dimensionality of the embeddings and transformer hidden states
-    intermediate_dim: int  # Dimensionality of the intermediate layer in the feed-forward network
     vocab_size: int  # Size of the vocabulary (number of unique tokens)
     max_seq_len: int  # Maximum sequence length that the model can handle
     attn_dropout: float  # Dropout rate for attention layers
@@ -21,6 +20,8 @@ class LLM_Args:
     rope_base: int  # Base value for Rotary Position Embedding (RoPE)
     num_kv_heads: Optional[int] = None  # Number of key-value attention heads (optional)
     attention_bias = False  # Boolean flag to indicate whether to use attention bias
+    intermediate_dim: Optional[int] = None  # Dimensionality of the intermediate layer in the feed-forward network
+
 
 
 # Next Define the Attention Block
@@ -33,7 +34,7 @@ class SelfAttention(nn.Module):
         super().__init__()
 
         # Validate that the number of attention heads is divisible by the number of key-value heads
-        if args.num_heads % args.num_kv_heads != 0:
+        if args.num_kv_heads and args.num_heads % args.num_kv_heads != 0:
             raise ValueError(
                 f"Number of attention heads ({args.num_heads}) must be divisible by the number of key-value attention heads ({args.num_kv_heads})"
             )
@@ -141,8 +142,7 @@ class SelfAttention(nn.Module):
             k,
             v,
             attn_mask=mask,
-            dropout_p=self.attn_dropout,
-            is_causal=self.kv_cache is None,
+            dropout_p=self.attn_dropout
         )  # output shape: (batch_size, num_heads, seq_len, head_dim)
 
         # Reshape the output back to the original shape
@@ -401,6 +401,8 @@ class TransformerEncoder(nn.Module):
                 h
             ).float()  # The shape remains [batch_size, seq_length, embedding_dim]
 
+        return h
+
 
 # we are norm norm many places which is a RMS norm let's implement it
 
@@ -430,7 +432,8 @@ class RMS_Norm(nn.Module):
         """
         input_dtype = x.dtype  # Save the original data type of the input tensor
 
-        x = x.to(torch.float32)  # Convert input to float32 for stable computation
+        if x.dtype != torch.float32:
+            x = x.to(torch.float32)  # Convert input to float32 for stable computation
 
         # Compute the reciprocal of the root mean square of the input
         rms_a = torch.rsqrt(x.pow(2).mean(dim=-1, keepdim=True) + self.eps)

model/model.py
@@ -1,10 +1,9 @@
 from dataclasses import dataclass
 from typing import List
-from spacy.symbols import nn
 import torch
-from torch import Tensor
+from torch import Tensor, nn
 import math
-from llama import llama_encoder, LLM_Args
+from .llama import llama_encoder, LLM_Args
 from utils import create_attn_mask
 @dataclass
 class ModelConfig:
@@ -50,7 +49,6 @@ class TextEncoder(nn.Module):
 
         self.output_proj = nn.Linear(args.hidden_channels, args.hidden_channels * 2, bias=False)
     def forward(self, x: Tensor, x_lengths: Tensor):
-        x = self.emb(x) * math.sqrt(self.args.hidden_channels)
         attn_mask = create_attn_mask(x_lengths)
         x = self.encoder(x, attn_mask)
         stats = self.output_proj(x)
@@ -67,4 +65,9 @@ class Generator(nn.Module):
 
 
 class MultiPeriodDiscriminator(nn.Module):
-    pass
+    def __init__(self):
+        super().__init__()
+        self.test = nn.Linear(64, 64)
+
+    def forward(self, x: Tensor, x_lengths: Tensor):
+        return x

train.py
@@ -7,11 +7,11 @@ from model.model import Generator, MultiPeriodDiscriminator
 from accelerate import Accelerator
 from transformers import get_cosine_schedule_with_warmup
 
-from utils import setup_logger
+from utils import setup_logger, get_vocab_len
 logger = setup_logger('model_training', 'train.log')
 
 
-torch.backends.cuda.enable_flash_sdp()
+torch.backends.cuda.enable_flash_sdp(True)
 torch.backends.cudnn.benchmark = False
 torch.backends.cudnn.deterministic = False
 torch.backends.cuda.matmul.allow_tf32 = True
@@ -31,8 +31,13 @@ accelerator = Accelerator()
 train_Dataset = AudioTextDataset(config.data.training_files, data_config)
 
 
-def train_and_evaluate():
-    pass
+def train_and_evaluate(loaders, nets):
+    train_loader, _ = loaders
+    net_g, net_d = nets
+    for batch_idx, (x, x_lengths, spec, spec_lengths, y, y_lengths) in enumerate(train_loader):
+        net_g(x, x_lengths)
+
+
 
 if accelerator.is_main_process:
     logger.info(config)
@@ -55,13 +60,11 @@ train_loader = DataLoader(
 
 
 net_g = Generator(
-    config.data.filter_length // 2 + 1,
-    config.train.segment_size // config.data.hop_length,
-    n_speakers=config.data.n_speakers,
-    **config.model,
+    get_vocab_len(config.data.lang),
+    config.model
 )
 
-net_d = MultiPeriodDiscriminator(config.model.use_spectral_norm)
+net_d = MultiPeriodDiscriminator()
 for name, param in net_g.named_parameters():
     if not param.requires_grad:
         print(name, "not requires_grad")
@@ -145,9 +148,13 @@ rank = accelerator.process_index
 for epoch in range(epoch_str, config.train.epochs + 1):
     if accelerator.is_main_process:
         train_and_evaluate(
+            [train_loader, None],
+            [net_g, None]
         )
     else:
         train_and_evaluate(
+            [train_loader, None],
+            [net_g, net_d]
         )
 
 # wandb.finish()

utils.py
@@ -5,6 +5,7 @@ import numpy as np
 import logging
 import sys
 from logging.handlers import RotatingFileHandler
+from lang_cleaner.eng import get_english_symbols
 
 def cleaned_text_to_sequence(cleaned_text: str, lang: str):
     """Converts a string of text to a sequence of IDs corresponding to the symbols in the text.
@@ -59,16 +60,23 @@ def intersperse(lst, item):
 def create_attn_mask(x_lengths: torch.Tensor):
     max_len = x_lengths.max().item()
     batch_size = x_lengths.size(0)
-    mask = torch.arange(max_len).expand(batch_size, max_len).to(x_lengths.device)
-    mask = mask < x_lengths.unsqueeze(1)  # [B, max_len]
 
-    mask = mask.unsqueeze(1) & mask.unsqueeze(2)
+    # Create a mask of shape [B, max_len]
+    mask = torch.arange(max_len, device=x_lengths.device).expand(batch_size, max_len)
+    mask = mask < x_lengths.unsqueeze(1)
 
-    # convert boolean mask to float mask where 0.0 means masked
+    # Convert to float
+    mask = mask.float()
 
-    mask = mask.float().masked_fill(mask == 0, float("-inf"))
+    # Transform the mask
+    # Use torch.finfo(mask.dtype).min instead of float("-inf") for better numerical stability
+    transformed_mask = (1.0 - mask) * torch.finfo(mask.dtype).min
 
-    return mask
+    # The mask should be broadcastable to [B, 1, max_len, max_len]
+    # for compatibility with scaled_dot_product_attention
+    transformed_mask = transformed_mask.unsqueeze(1).unsqueeze(1)
+
+    return transformed_mask
 
 
 def setup_logger(name, log_file, level=logging.INFO):
@@ -94,3 +102,10 @@ def setup_logger(name, log_file, level=logging.INFO):
     logger.addHandler(f_handler)
 
     return logger
+
+def get_vocab_len(lang: str):
+    if lang == "en":
+        symbols, _, _ = get_english_symbols()
+        return len(symbols)
+    else:
+        raise ValueError(f"Language {lang} is not supported.")